abf259691129498dc171f429df114fe0fef27419
DarthHaran <jaksasith@gmail.com>
2021-03-26 15:56:52 +0100

    Updated pagination for the likes

--- API/Interfaces/ILikesRepository.cs
+++ API/Interfaces/ILikesRepository.cs
@@ -1,6 +1,6 @@
 ﻿using API.DTOs;
 using API.Entities;
-using System.Collections.Generic;
+using API.Helpers;
 using System.Threading.Tasks;
 
 namespace API.Interfaces
@@ -9,6 +9,6 @@ namespace API.Interfaces
     {
         Task<UserLike> GetUserLike(int sourceUserId, int likedUserId);
         Task<AppUser> GetUserWithLikes(int userId);
-        Task<IEnumerable<LikeDto>> GetUserLikes(string predicate, int userId);
+        Task<PagedList<LikeDto>> GetUserLikes(LikesParams likesParams);
     }
 }
